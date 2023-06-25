24f0e19f09efc0aa4fa9e95172d375711fe1b976
Johanna <112203192+Johtro97@users.noreply.github.com>
2023-06-25 10:54:19 +0200

    layout

--- main.js
+++ main.js
@@ -1,14 +1,38 @@
+let btnUpdate = document.getElementById("btn-update");
 
-async function getWeather(){
-    try {
-        let response = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=59.329323&lon=18.068581&appid=b8acbadd48a65146362de9416be1b620&units=metric") 
-        let weatherData = await response.json();
-        console.log(weatherData);
-    }
-
-    catch (error){
-        console.log('Error:', error)
-    }
+function fetchWeather(city){
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=b8acbadd48a65146362de9416be1b620`)
+    .then((res) => res.json())
+    .then((data) => {   
+      let weatherData = data;
+      displayWeather(weatherData);
+    });
 }
 
-getWeather();
+function displayWeather(weatherData){
+  let weatherWrapper = document.querySelector(".weather-wrapper");
+  let table = document.querySelector("#weather-table");
+  let city = document.querySelector(".city");
+  let temp = weatherWrapper.querySelector("#temp");
+  let description = weatherWrapper.querySelector("#description");
+  let pressure = weatherWrapper.querySelector("#pressure");
+  let humidity = weatherWrapper.querySelector("#humidity");
+  let wind = weatherWrapper.querySelector("#windspeed");
+
+  city.innerText = weatherData.name + ", " + weatherData.sys.country;
+  temp.innerText = weatherData.main.temp.toFixed(1) + " °C";
+  description.innerText = weatherData.weather[0]['description'];
+  pressure.innerText = weatherData.main.pressure + " hPa";
+  humidity.innerText = weatherData.main.humidity + " %";
+  wind.innerText = weatherData.wind.speed + " m/s";
+  
+  weatherWrapper.append(city, temp, description, table);
+  table.append(pressure, humidity, wind);
+  
+  //refresh page when user press button
+  btnUpdate.addEventListener('click', () => {
+    location.reload(); 
+  })
+}
+
+fetchWeather("stockholm");
